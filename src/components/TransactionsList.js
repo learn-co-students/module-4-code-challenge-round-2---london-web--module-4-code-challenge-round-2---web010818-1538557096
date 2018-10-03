a516c9a4d95c43b43a777106ba8c7d9524d27590
kreyes12 <karla.y.reyes12@gmail.com>
2018-10-03 10:47:43 +0100

    code challenge complete

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,35 +1,39 @@
 import React from 'react'
+import Transaction from './Transaction'
 
-const TransactionsList = () => {
+const TransactionsList = (props) => {
+  const displayTransactions = () => {
+    return props.transactions.map(transaction => <Transaction
+      transaction={transaction} key={transaction.id} />
+    )
+  }
 
   return (
-    <table className="ui celled striped padded table">
+    <table className='ui celled striped padded table'>
       <tbody>
         <tr>
           <th>
-            <h3 className="ui center aligned header">
+            <h3 className='ui center aligned header'>
               Posted At
             </h3>
           </th>
           <th>
-            <h3 className="ui center aligned header">
+            <h3 className='ui center aligned header'>
               Description
             </h3>
           </th>
           <th>
-            <h3 className="ui center aligned header">
+            <h3 className='ui center aligned header'>
               Category
             </h3>
           </th>
           <th>
-            <h3 className="ui center aligned header">
+            <h3 className='ui center aligned header'>
               Amount
             </h3>
           </th>
         </tr>
-
-        {"... your code here..."}
-
+        {displayTransactions()}
       </tbody>
     </table>
   )
