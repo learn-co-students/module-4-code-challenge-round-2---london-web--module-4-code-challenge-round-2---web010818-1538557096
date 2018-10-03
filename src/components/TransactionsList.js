f91415f840dfd95a743166e5201dc9e0748344f1
github username <github email address>
2018-10-03 10:48:51 +0100

    ra

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,6 +1,6 @@
 import React from 'react'
-
-const TransactionsList = () => {
+import Transaction from './Transaction'
+const TransactionsList = (props) => {
 
   return (
     <table className="ui celled striped padded table">
@@ -28,8 +28,13 @@ const TransactionsList = () => {
           </th>
         </tr>
 
-        {"... your code here..."}
-
+        {
+          props.transactions.map(transaction =>
+            <Transaction
+              transaction={transaction}
+            />
+          )
+        }
       </tbody>
     </table>
   )
